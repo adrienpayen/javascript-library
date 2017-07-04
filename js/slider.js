1473b0d7b167ba9b08e15d095dde44bdea34fc23
Wildrav <mayoute.annise@gmail.com>
2017-07-04 10:33:47 +0200

    autoplayHover, il manque le stop quand on passe en OFF.

--- js/slider.js
+++ js/slider.js
@@ -1,4 +1,4 @@
-(function($){
+(function($) {
 
     $.fn.extend({
 
@@ -6,16 +6,16 @@
 
             //Settings list and the default values
             var defaults = {
-                jsonData : {
-                    url : false
+                jsonData: {
+                    url: false
                 },
-                jsonDataStruct : {
-                    url : "url",
-                    title : "title",
-                    description : "description"
+                jsonDataStruct: {
+                    url: "url",
+                    title: "title",
+                    description: "description"
                 },
-                mainDiv : false,
-                speed : 1500,
+                mainDiv: false,
+                speed: 1500,
                 interval: 4500,
                 autoplay: true
             };
@@ -40,11 +40,11 @@
                         url: o.jsonData.url,
                         type: 'GET',
 
-                        success : function(content) {
+                        success: function(content) {
                             var parsedContent = JSON.parse(content);
 
-                            parsedContent.forEach(function(element){
-                                Slider.images.add(mainDiv,element, o.jsonDataStruct, nbImg);
+                            parsedContent.forEach(function(element) {
+                                Slider.images.add(mainDiv, element, o.jsonDataStruct, nbImg);
                                 nbImg++;
                             });
 
@@ -57,12 +57,58 @@
 
                             );
 
-                            $(".next").on("click", function(){ Slider.nextStep(mainDiv, o.speed, nbImg) });
-                            $(".previous").on("click", function(){ Slider.previousStep(mainDiv, o.speed, nbImg) });
-                            $(".play").on("click", function(){ Slider.autoPlay(mainDiv, o.speed, o.interval) });
-                            $(".stop").on("click", function(){ Slider.stopAutoPlay() });
-                            $(".next").on("click", function(){ $(".textSlider").slideDown() });
-                 $('.sliderPuces span').on("click", function(){
+                            $(".next").on("click", function() {
+                                Slider.nextStep(mainDiv, o.speed, nbImg)
+                            });
+                            $(".previous").on("click", function() {
+                                Slider.previousStep(mainDiv, o.speed, nbImg)
+                            });
+                            $(".play").on("click", function() {
+                                Slider.autoPlay(mainDiv, o.speed, o.interval)
+                            });
+                            $(".stop").on("click", function() {
+                                Slider.stopAutoPlay()
+                            });
+                            $(".next").on("click", function() {
+                                $(".textSlider").slideDown()
+                            });
+                            $(".autoPlayHover").ready(function() {
+
+                                var $on = 'section';
+                                $($on).css({
+                                    'background': 'none',
+                                    'border': 'none',
+                                    'box-shadow': 'none'
+                                });
+
+                                $("#autoPlayHover").click(function(){
+                                            if($(this).prop("checked") == true){
+                                                alert("Checkbox is checked.");
+      
+                                                $(".sliderNav").hover(function(){
+
+                                                    Slider.stopAutoPlay()
+
+                                                       }, function(){
+                                                         
+                                                          Slider.autoPlay(mainDiv, o.speed, o.interval)
+                                                   });
+                                            } 
+                                            else if ($(this).prop("checked") == false){
+
+                                                 $('.sliderNav').(function(){
+
+                                                     Slider.stopAutoPlay() // annule l'action du lien
+                                                 });
+
+                                            }
+                                     
+                                        });
+
+                            });
+
+
+                            $('.sliderPuces span').on("click", function() {
                                 Slider.puceSlide(mainDiv, o.speed, $(this).data('target'), nbImg)
                             })
 
@@ -83,7 +129,7 @@ var Slider = {
     },
 
     resize: function(target, numberElement) {
-        $("#"+target).css('width', (100 * numberElement) + "%");
+        $("#" + target).css('width', (100 * numberElement) + "%");
 
         // $(window).resize(function(){
         //
@@ -91,22 +137,22 @@ var Slider = {
         // })
     },
 
-    images : {
+    images: {
         add: function(target, content, struct, id) {
             if (id == '2') {
                 $("#" + target).append(
-                    "<li id='"+guid()+"' class='active' data-id='"+ id +"'>" +
-                    "<img src='"+content[struct.url]+"' style='width:100%; margin:auto;' alt='"+content[struct.description]+"'>" +
-                    "<div class='textSlider'><p class='contentTextSlider'><h2 class='title'>"+content[struct.title]+"</h2>" + content[struct.description] +"</p></div>" +
+                    "<li id='" + guid() + "' class='active' data-id='" + id + "'>" +
+                    "<img src='" + content[struct.url] + "' style='width:100%; margin:auto;' alt='" + content[struct.description] + "'>" +
+                    "<div class='textSlider'><p class='contentTextSlider'><h2 class='title'>" + content[struct.title] + "</h2>" + content[struct.description] + "</p></div>" +
                     "</li>");
-                $('.sliderPuces').append("<span class='active' data-target='"+ id +"'></span>");
+                $('.sliderPuces').append("<span class='active' data-target='" + id + "'></span>");
             } else {
                 $("#" + target).append(
-                    "<li id='"+guid()+"' data-id='"+ id +"'>" +
-                    "<img src='"+content[struct.url]+"' style='width:100%; margin:auto;' alt='"+content[struct.description]+"'>" +
-                    "<div class='textSlider'><p class='contentTextSlider'><h2 class='title'>"+content[struct.title]+"</h2>" + content[struct.description] +"</p></div>" +
+                    "<li id='" + guid() + "' data-id='" + id + "'>" +
+                    "<img src='" + content[struct.url] + "' style='width:100%; margin:auto;' alt='" + content[struct.description] + "'>" +
+                    "<div class='textSlider'><p class='contentTextSlider'><h2 class='title'>" + content[struct.title] + "</h2>" + content[struct.description] + "</p></div>" +
                     "</li>");
-                $('.sliderPuces').append("<span data-target='"+ id +"'></span>");
+                $('.sliderPuces').append("<span data-target='" + id + "'></span>");
             }
         }
 
@@ -114,7 +160,7 @@ var Slider = {
 
     nextStep: function(target, speed, nbImg) {
         var idActive = $("li.active").removeClass("active").data('id');
-        $("span[data-target="+idActive+"]").removeClass("active");
+        $("span[data-target=" + idActive + "]").removeClass("active");
 
         if (idActive == nbImg) {
             idActive = 1;
@@ -122,16 +168,20 @@ var Slider = {
             idActive++;
         }
 
-        $("li[data-id="+idActive+"]").addClass("active");
-        $("span[data-target="+idActive+"]").addClass("active");
-        $("#"+target).animate({marginLeft:-($("#"+target).children().width())*2},speed,function(){
-            $(this).css({marginLeft:"-"+$("#"+target).children().width()+"px"}).find("li:last").after($(this).find("li:first"));
+        $("li[data-id=" + idActive + "]").addClass("active");
+        $("span[data-target=" + idActive + "]").addClass("active");
+        $("#" + target).animate({
+            marginLeft: -($("#" + target).children().width()) * 2
+        }, speed, function() {
+            $(this).css({
+                marginLeft: "-" + $("#" + target).children().width() + "px"
+            }).find("li:last").after($(this).find("li:first"));
         });
     },
 
     previousStep: function(target, speed, nbImg) {
         var idActive = $("li.active").removeClass("active").data('id');
-        $("span[data-target="+idActive+"]").removeClass("active");
+        $("span[data-target=" + idActive + "]").removeClass("active");
 
         if (idActive == 1) {
             idActive = nbImg;
@@ -139,17 +189,21 @@ var Slider = {
             idActive--;
         }
 
-        $("li[data-id="+idActive+"]").addClass("active");
-        $("span[data-target="+idActive+"]").addClass("active");
+        $("li[data-id=" + idActive + "]").addClass("active");
+        $("span[data-target=" + idActive + "]").addClass("active");
 
-        $("#"+target).animate({marginLeft:0},speed,function(){
-            $(this).css({marginLeft:"-100%"}).find("li:first").before($(this).find("li:last"));
+        $("#" + target).animate({
+            marginLeft: 0
+        }, speed, function() {
+            $(this).css({
+                marginLeft: "-100%"
+            }).find("li:first").before($(this).find("li:last"));
         })
     },
 
     autoPlay: function(target, speed, interval) {
         Slider.nextStep(target, speed);
-        myInterval = setInterval(function(){
+        myInterval = setInterval(function() {
             Slider.nextStep(target, speed)
         }, interval);
     },
@@ -160,7 +214,7 @@ var Slider = {
 
     puceSlide: function(target, speed, data, nbImg) {
         $("span.active").removeClass("active");
-        $("span[data-target="+data+"]").addClass("active");
+        $("span[data-target=" + data + "]").addClass("active");
 
         var idActive = $("li.active").data('id');
 
@@ -168,33 +222,41 @@ var Slider = {
             var space = data - idActive;
 
             $("li.active").removeClass('active');
-            $("li[data-id="+data+"]").addClass('active');
+            $("li[data-id=" + data + "]").addClass('active');
 
-            $("#"+target).animate({marginLeft:(-100*(1+space)) + "%"},speed,function(){
+            $("#" + target).animate({
+                marginLeft: (-100 * (1 + space)) + "%"
+            }, speed, function() {
 
                 for (var i = 0; i < space; i++) {
-                    $("#"+target).find("li:last").after($(this).find("li:first"));
+                    $("#" + target).find("li:last").after($(this).find("li:first"));
                 }
 
-                $("#"+target).css({marginLeft:"-100%"});
+                $("#" + target).css({
+                    marginLeft: "-100%"
+                });
             });
         } else if (idActive > data) {
             var space = idActive - data;
 
             $("li.active").removeClass('active');
-            $("li[data-id="+data+"]").addClass('active');
+            $("li[data-id=" + data + "]").addClass('active');
 
-            $("#"+target).animate({marginLeft:(100*(space-1)) + "%"},speed,function(){
+            $("#" + target).animate({
+                marginLeft: (100 * (space - 1)) + "%"
+            }, speed, function() {
                 for (var i = 0; i < space; i++) {
-                    $("#"+target).find("li:first").before($(this).find("li:last"));
+                    $("#" + target).find("li:first").before($(this).find("li:last"));
                 }
-                $(this).css({marginLeft:"-100%"});
+                $(this).css({
+                    marginLeft: "-100%"
+                });
             });
         }
     }
 };
 
-    // run the currently selected effect
+// run the currently selected effect
 
 /** Helpers Function **/
 function guid() {
